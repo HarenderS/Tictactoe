3d90057f11358d671d72addaa0d7ccdb4cf2c2ed
HarenderS <hs.dhanoya@alumnos.urjc.es>
2020-11-30 05:02:33 +0100

    Add test setTurnAndGetNotNullMementoTest and the code for it

--- tictactoe/src/main/java/TicTacToe/tictactoe/models/GameMemento.java
+++ tictactoe/src/main/java/TicTacToe/tictactoe/models/GameMemento.java
@@ -3,13 +3,15 @@ package TicTacToe.tictactoe.models;
 public class GameMemento {
 
 	private Board board;
+	private Turn turn;
 	
-	public GameMemento(Board board) {
+	public GameMemento(Board board, Turn turn) {
 		this.board = board;
+		this.turn = turn;
 	}
 
 	public Turn getTurn() {
-		return null;
+		return this.turn;
 	}
 
 	public Board getBoard() {
--- tictactoe/src/test/java/TicTacToe/tictactoe/models/GameMementoTest.java
+++ tictactoe/src/test/java/TicTacToe/tictactoe/models/GameMementoTest.java
@@ -1,31 +1,50 @@
 package TicTacToe.tictactoe.models;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import TicTacToe.tictactoe.types.PlayerType;
+import TicTacToe.tictactoe.types.Token;
 
 public class GameMementoTest {
 
+	private Board board;
+	private Player[] player;
+	private Turn turn;
+	
+	@Before
+	public void before() {
+		this.board = new Board();
+		this.player = new Player[2];
+        this.player[0] = new Player(Token.values()[0], this.board, PlayerType.USER_PLAYER);
+        this.player[1] = new Player(Token.values()[1], this.board, PlayerType.MACHINE_PLAYER);
+        this.turn = new Turn(this.player);
+	}
 	
 	@Test
 	public void getBoardNullMementoTest() {
-		GameMemento gameMemento = new GameMemento(null);
+		GameMemento gameMemento = new GameMemento(null,null);
 		assertNull(gameMemento.getBoard());
-		
 	}
 	
 	@Test
 	public void getTurnNullMementoTest() {
-		GameMemento gameMemento = new GameMemento(new Board());
+		GameMemento gameMemento = new GameMemento(null,null);
 		assertNull(gameMemento.getTurn());
-		
 	}
 	
 	@Test
-	public void setBoardAndGetNotNullMementoTest() {
-		GameMemento gameMemento = new GameMemento(new Board());
+	public void setBoardNullMementoTest() {
+		GameMemento gameMemento = new GameMemento(this.board,this.turn);
 		assertNotNull(gameMemento.getBoard());
-		
+	}
+	
+	@Test
+	public void setTurnAndGetNotNullMementoTest() {
+		GameMemento gameMemento = new GameMemento(this.board,this.turn);
+		assertNotNull(gameMemento.getBoard());
 	}
 }
