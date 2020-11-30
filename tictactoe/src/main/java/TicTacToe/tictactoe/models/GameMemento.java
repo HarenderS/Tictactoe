eb4a81c2762bbed18495c949175d1e134ccd0432
HarenderS <hs.dhanoya@alumnos.urjc.es>
2020-11-30 04:52:02 +0100

    Add the test setBoardAndGetNotNullMementoTest and the Code for it

--- tictactoe/src/main/java/TicTacToe/tictactoe/models/GameMemento.java
+++ tictactoe/src/main/java/TicTacToe/tictactoe/models/GameMemento.java
@@ -2,13 +2,18 @@ package TicTacToe.tictactoe.models;
 
 public class GameMemento {
 
+	private Board board;
+	
+	public GameMemento(Board board) {
+		this.board = board;
+	}
 
 	public Turn getTurn() {
 		return null;
 	}
 
 	public Board getBoard() {
-		return null;
+		return this.board;
 	}
 
 }
--- tictactoe/src/test/java/TicTacToe/tictactoe/models/GameMementoTest.java
+++ tictactoe/src/test/java/TicTacToe/tictactoe/models/GameMementoTest.java
@@ -1,22 +1,31 @@
 package TicTacToe.tictactoe.models;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.Test;
 
 public class GameMementoTest {
 
+	
 	@Test
 	public void getBoardNullMementoTest() {
-		GameMemento gameMemento = new GameMemento();
+		GameMemento gameMemento = new GameMemento(null);
 		assertNull(gameMemento.getBoard());
 		
 	}
 	
 	@Test
 	public void getTurnNullMementoTest() {
-		GameMemento gameMemento = new GameMemento();
+		GameMemento gameMemento = new GameMemento(new Board());
 		assertNull(gameMemento.getTurn());
+		
+	}
+	
+	@Test
+	public void setBoardAndGetNotNullMementoTest() {
+		GameMemento gameMemento = new GameMemento(new Board());
+		assertNotNull(gameMemento.getBoard());
 		
 	}
 }
